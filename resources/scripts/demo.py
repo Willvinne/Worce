a5730c7555873e7c43dcb388a22cb0ce1796b70e
Willvinne <94703673+Willvinne@users.noreply.github.com>
2025-02-16 12:46:48 +0300

    Update demo.py

--- resources/scripts/demo.py
+++ resources/scripts/demo.py
@@ -3,6 +3,6 @@ import os, sys
 print("This is a demo script.")
 command = input("Do you want open scripts folder? [y/n]: ")
 if (command == "y"):
-    os.startfile(filepath="scripts")
+    os.startfile(filepath="resources/scripts")
 else:
     sys.exit()
